6a4f10d6f92b7ca782ca93a1f937b334a3e22f12
Marion <mariontamnai@gmail.com>
2026-05-18 16:54:13 +0300

    moved the logout btn from bottom to top

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -134,6 +134,16 @@ const Dashboard = () => {
             <h2>Admin Dashboard</h2>
             <p className="helper-text">Welcome, {admin?.name}</p>
 
+            <button
+  className="btn btn-outline"
+  onClick={() => {
+    sessionStorage.clear();
+    navigate('/');
+  }}
+>
+  LOGOUT
+</button>
+
             {message && (
               <div className={`alert alert-${messageType}`}>{message}</div>
             )}
@@ -189,15 +199,7 @@ const Dashboard = () => {
                   )}
                 </div>
 
-                <button
-  className="btn btn-outline"
-  onClick={() => {
-    sessionStorage.clear();
-    navigate('/');
-  }}
->
-  LOGOUT
-</button>
+                
                 
               </>
             )}
